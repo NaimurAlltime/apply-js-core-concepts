c71d0a79a49ef2608b1d988bf74715b186f0d4f0
Naimur Rahman <naimur2231@gmail.com>
2023-01-31 20:59:24 +0600

    var let const er diffrent

---
+++ var-let-const.js
@@ -0,0 +1,10 @@
+var age = 22;
+var age = 23;
+
+let myFriend = 'Shovo';
+// let a same name variable 2nd time declare kora jayna 
+// let myFriend = 'shishir'; 
+
+const name = 'Naimur Rahman';
+// const er value sobsomay uniqe hobe  
+// name = 'Tushar Ahammed';
